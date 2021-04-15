3183a67840af50045b7abb47f84951a9e6557e4d
root <root@localhost.localdomain>
2021-04-15 17:35:34 +0800

    更新于 04-15 17:35

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -15,11 +15,28 @@ if [ ! -d $JD_DIR/thirdpard ]; then
   mkdir -p /jd/thirdpard
 fi
 
+crond
+
 echo -e "\n======================== 2. 更新源代码 ========================\n"
 jd update
 echo
 
-echo -e "========================3. 启动控制面板========================\n"
+crontab $JD_DIR/config/crontab.list
+
+echo -e "========================3. 启动挂机程序========================\n"
+if [[ $ENABLE_HANGUP == true ]]; then
+  . $JD_DIR/config/cookie.sh
+  if [ -n "${Cookie1}" ]; then
+    jd hangup
+    echo -e "挂机程序启动成功...\n"
+  else
+    echo -e "config.sh中还未填入有效的Cookie，可能是首次部署容器，因此不启动挂机程序...\n"
+  fi
+elif [[ $ENABLE_HANGUP == false ]]; then
+  echo -e "已设置为不自动启动挂机程序，跳过...\n"
+fi
+
+echo -e "========================4. 启动控制面板========================\n"
 if [[ $ENABLE_WEB_PANEL == true ]]; then
   jd panelon
   jd panelon
@@ -28,6 +45,8 @@ elif [[ $ENABLE_WEB_PANEL == false ]]; then
 fi
 echo -e "容器启动成功...\n"
 
-crond -f
+if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ]; then
+  set -- node "$@"
+fi
 
 exec "$@"
--- docker/docker/docker-entrypoint.sh
+++ docker/docker/docker-entrypoint.sh
@@ -17,7 +17,6 @@ fi
 
 crond
 
-
 echo -e "\n======================== 2. 更新源代码 ========================\n"
 jd update
 echo
@@ -28,7 +27,7 @@ echo -e "========================3. 启动挂机程序========================\n
 if [[ $ENABLE_HANGUP == true ]]; then
   . $JD_DIR/config/cookie.sh
   if [ -n "${Cookie1}" ]; then
-    bash $$JD_DIR/jd.sh hangup
+    jd hangup
     echo -e "挂机程序启动成功...\n"
   else
     echo -e "config.sh中还未填入有效的Cookie，可能是首次部署容器，因此不启动挂机程序...\n"
@@ -36,7 +35,6 @@ if [[ $ENABLE_HANGUP == true ]]; then
 elif [[ $ENABLE_HANGUP == false ]]; then
   echo -e "已设置为不自动启动挂机程序，跳过...\n"
 fi
-
 
 echo -e "========================4. 启动控制面板========================\n"
 if [[ $ENABLE_WEB_PANEL == true ]]; then
