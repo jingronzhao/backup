214c1d29617b98a72893cbb227948429a5cd4709
root <root@localhost.localdomain>
2021-04-15 13:02:04 +0800

    2021-04-15 13:02:02:

--- repair.sh
+++ repair.sh
@@ -9,7 +9,7 @@ dir_shell=$(dirname $(readlink -f "$0"))
 dir_root=$dir_shell
 dir_config=$dir_root/config
 file_cookie=$dir_config/cookie.sh
-
+[ -f /proc/1/cgroup ] && [[ -n $(cat /proc/1/cgroup | grep cpuset | grep scope) ]] && echo "docker无法使用此命令，抱歉" && exit 0
 [ -s $file_cookie ] && cp $file_cookie $(dirname $dir_shell)/cookie.sh && echo "备份cookie成功"
 pkill -9 node
 bash $dir_shell/jd.sh paneloff
@@ -24,7 +24,7 @@ function REINSTALLATION() {
 
   echo -e "\n2. 还原配置文件"
   mkdir -p $dir_config
-  [ -f $(dirname $dir_shell)/cookie.sh ] && cp $(dirname $dir_shell)/cookie.sh $file_cookie && rm -rf $(dirname $dir_shell)/cookie.sh
+  [ -f $(dirname $dir_shell)/cookie.sh ] && cp -rf $(dirname $dir_shell)/cookie.sh $file_cookie && rm -rf $(dirname $dir_shell)/cookie.sh && echo "还原配置文件成功"
 
   echo -e "\n3. 执行脚本更新以及定时文件更新"
   npm config set registry https://registry.npm.taobao.org
